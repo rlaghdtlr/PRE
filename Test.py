997d2aaf65baf56ad784441172879478c43fdc4b
rlaghdtlr <rlaghdtlrjkl@naver.com>
2023-09-21 14:22:57 +0900

    오토 폴더 만들기 커밋

--- Test.py
+++ Test.py
@@ -1,6 +1,10 @@
-A, B = map(int, input().split())
+import pandas as pd
+import os
 
-def alpha (x,y):
-    return(x+y)*(x-y)
+excel_file = 'data.xlsx'  # 엑셀 파일 경로
+df = pd.read_excel(excel_file)  # 엑셀 파일 읽기
 
-print(alpha(A,B))
+for index, row in df.iterrows():
+    folder_name = str(row['파일명'])  # 폴더 이름 열의 이름을 적절히 변경하세요
+    folder_path = os.path.join(r'C:\Users\CTLASSISTANT\Desktop\표정리', folder_name)  # 폴더를 생성할 기본 경로를 지정하세요
+    os.makedirs(folder_path, exist_ok=True)  # 폴더를 생성하고 이미 존재할 경우 오류를 방지합니다.
